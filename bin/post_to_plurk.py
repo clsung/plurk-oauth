0583d2cdd1e6d26eaabe4d8fa05ee10160872e24
Cheng-Lung Sung <clsung@gmail.com>
2011-05-23 16:04:07 +0800

    add post_to_plurk

---
+++ bin/post_to_plurk.py
@@ -0,0 +1,39 @@
+import sys
+sys.path.append('plurk-oauth/')
+from PlurkAPI import PlurkAPI
+import getopt
+import json
+
+def usage():
+    print '''Help Information:
+    -h: Show help information
+    '''
+
+if __name__ == '__main__':
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "ho:v", ["help", "output="])
+    except getopt.GetoptError, err:
+        print str(err) # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+    file = open('API.keys', 'r+')
+    data = json.load(file)
+    plurk = PlurkAPI(data["CONSUMER_KEY"], data["CONSUMER_SECRET"])
+    if data.get('ACCESS_TOKEN'):
+        plurk.authorize(data["ACCESS_TOKEN"],data["ACCESS_TOKEN_SECRET"])
+    else:
+        plurk.authorize()
+        data["ACCESS_TOKEN"] = plurk._oauth.oauth_token['oauth_token']
+        data["ACCESS_TOKEN_SECRET"] = plurk._oauth.oauth_token['oauth_token_secret']
+        json.dump(data,file)
+
+    content = 'Test from Plurk OAuth API'
+    if len(sys.argv) > 1:  
+        content = sys.argv[1]
+    qualifier = 'says'
+    if len(sys.argv) > 2:  
+        qualifier = sys.argv[2]
+    print plurk.callAPI('/APP/Timeline/plurkAdd', {
+        'content': content,
+        'qualifier': qualifier } )
+    
